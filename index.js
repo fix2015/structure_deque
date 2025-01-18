b0d87a0422d4b08c7c7886cb321075133d867496
Semianchuk Vitalii <fix20152@gmail.com>
2025-01-18 14:21:48 +0000

    Create task deque

---
+++ index.js
@@ -0,0 +1,45 @@
+class Deque {
+    constructor() {
+        
+    }
+
+    // Add an element to the front of the deque
+    insertFront(element) {
+       
+    }
+
+    // Add an element to the back of the deque
+    insertBack(element) {
+       
+    }
+
+    // Remove and return the element from the front of the deque
+    removeFront() {
+       
+    }
+
+    // Remove and return the element from the back of the deque
+    removeBack() {
+       
+    }
+
+    // Peek at the element at the front without removing it
+    peekFront() {
+       
+    }
+
+    // Peek at the element at the back without removing it
+    peekBack() {
+      
+    }
+
+    // Get the size of the deque
+    size() {
+       
+    }
+
+    // Check if the deque is empty
+    isEmpty() {
+        
+    }
+}
